ff2e46c34779fdff5ef6bd7280532ae3b5f8d75e
JaydenDev <willsjayden2@gmail.com>
2022-03-19 15:13:54 -0500

    upd

--- requestHandler.js
+++ requestHandler.js
@@ -1,7 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search);
-const app = urlParams.get('app');
 
 // if app = wsite go redirect to https://jaydendev.github.io
-if (app === 'wsite') {
+if (urlParams.get('app') == 'wsite') {
     window.location.replace('https://jaydendev.github.io');
+    const stat = "redir-wsite";
 }
+
+console.log("test " + stat)
