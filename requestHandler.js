726e0e5e8c9bf63e5df823f446995c1e18512a91
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2022-03-20 18:28:55 -0500

    Add halt procedure

--- requestHandler.js
+++ requestHandler.js
@@ -5,7 +5,6 @@ if (urlParams.get('app') == 'wsite') {
     window.location.replace('https://jaydendev.github.io');
     const stat = "redir-wsite";
 }
-
-console.log("test " + stat)
-console.log(app)
-console.log(urlParams)
+if (urlParams.get('proc') == 'halt') {
+    window.location.replace('#');
+}
